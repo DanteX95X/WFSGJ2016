4b4e4da4bd2b4a5eec852402650eee68fe358c32
Dante <daniellewinski95@gmail.com>
2016-09-03 04:36:46 +0200

    Recomitted: Health script.

---
+++ WFSGJ2016/Assets/Scripts/Game/HealthScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    public int health = 100;
+    public Slider healthBar;
+    public AudioClip deathClip;
+    AudioSource audio;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (healthBar != null)
+        {
+            healthBar.value = health;
+            healthBar.maxValue = health;
+        }
+        audio = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void TakeDamage(int dmg)
+    {
+        health -= dmg;
+        if (healthBar != null)
+            healthBar.value = health;
+        if (health <= 0)
+            Die();
+    }
+
+    public void Die()
+    {
+        StartCoroutine("CDie");
+    }
+    IEnumerator CDie()
+    {
+        //Odpal animacje, odegraj dźwięk i odpal particle???
+        if (audio != null && deathClip != null)
+            audio.PlayOneShot(deathClip, 1f);
+        yield return new WaitForSeconds(0.5f);
+
+        Destroy(gameObject);
+    }
+
+
+}
