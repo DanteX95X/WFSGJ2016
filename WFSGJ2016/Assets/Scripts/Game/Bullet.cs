12ba11353c669725c21b5c72ba2f53c0c2384636
Dante <daniellewinski95@gmail.com>
2016-09-03 02:39:35 +0200

    Created: Bullet script, ranged player script, IMortal interface.

---
+++ WFSGJ2016/Assets/Scripts/Game/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    float movementSpeed = 15;
+
+    public GameObject ParentCharacter { get; set; }
+
+    float counter = 0;
+    void Start()
+    {
+        Debug.Log("Ranged player spawned");
+    }
+
+	void Update ()
+    {
+        transform.position += transform.up * movementSpeed * Time.deltaTime;
+        DestroyBullet();
+	}
+
+    void DestroyBullet()
+    {
+        counter += Time.deltaTime;
+        if (counter > 2)
+            Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject != ParentCharacter && (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy"))
+        {
+            collision.gameObject.GetComponent<IMortal>().Die();
+            Debug.Log("Character has been shot");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ WFSGJ2016/Assets/Scripts/Game/RangedPlayer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangedPlayer : MonoBehaviour, IMortal
+{
+    [SerializeField]
+    float rotationSpeed = 100;
+
+    [SerializeField]
+    float movementSpeed = 10;
+
+    [SerializeField]
+    GameObject bullet = null;
+	
+	void Update ()
+    {
+	    if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.up * movementSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += -transform.up * movementSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            (Instantiate(bullet, transform.position, transform.rotation) as GameObject).GetComponent<Bullet>().ParentCharacter = gameObject;
+        }
+	}
+
+    public void Die()
+    {
+        //TODO: Make it die
+    }
+}
