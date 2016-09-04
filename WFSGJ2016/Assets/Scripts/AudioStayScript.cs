363fb3f40ff6dca21e37edb042887990cf343cbd
Jacek <jacek.kozieja@gmail.com>
2016-09-04 08:54:33 +0200

    UI improvements

---
+++ WFSGJ2016/Assets/Scripts/AudioStayScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioStayScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioStayScript[] au = GameObject.FindObjectsOfType<AudioStayScript>();
+        if (au.Length>1)
+        {
+            for (int i = 1; i < au.Length;i++ )
+                Destroy(au[i].gameObject);
+        }
+        DontDestroyOnLoad(transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- WFSGJ2016/Assets/Scripts/MainMenuScript.cs
+++ WFSGJ2016/Assets/Scripts/MainMenuScript.cs
@@ -8,7 +8,7 @@ public class MainMenuScript : MonoBehaviour {
     AudioSource clickAudio;
 	void Start () {
         clickAudio = GetComponent<AudioSource>();
-        DontDestroyOnLoad(transform.gameObject);
+
 	
 	}
 	
